808f2f40524969e0a180df72a218931153a595d8
shahzod <shoxzodbek15@gmail.com>
2024-10-01 11:47:30 +0500

    function 10 gacha

---
+++ app.js
@@ -0,0 +1,104 @@
+// Func-1
+// function power(a, n) {
+//     return a ** n;
+
+// }
+// console.log(power(3,5));
+
+// Func-2
+// function mean(a, b){
+//     let arifmetik =  (a + b) / 2;
+//     let  geometric = Math.sqrt(a * b);
+//     return [arifmetik, geometric];
+// }
+// console.log(mean(12,48));
+
+// Func-3
+// function sign(n) {
+//     if (n > 0) {
+//         return 1;
+//     } else if (n < 0) {
+//         return -1;
+//     }else{
+//         return 0;
+//     }
+// }
+// console.log(sign(0));
+
+// Func-4
+// function numberOfRoots(A, B, C) {
+//     let D = B ** 2 - 4 * A * C;
+//     if (D > 0) {
+//         return 2;
+//     } else if (D == 0) {
+//         return 1;
+//     }else{
+//         return  0;
+//     }
+// }
+// console.log(numberOfRoots(1, -6, 9));
+
+// Func-5
+// function areaCircle(R) {
+//     let area = 3.14 * R ** 2;
+//     return area;
+// }
+//  console.log(areaCircle(5));
+
+// Func-6
+// function sumRange(A, B) {
+//     if (A > B) {
+//         return 0;
+//     }
+//     let sum = 0;
+//     for (let i = A; i <= B; i++) {
+//         sum += i;
+//     }
+//     return sum;
+
+// }
+// console.log(sumRange(8, 10));
+
+// Func-7
+// function calc(A, B, S) {
+//     switch (S) {
+//         case '+':
+//             return A + B;
+//         case '-':
+//             return A - B;
+//         case '*':
+//             return A * B;
+//         case '/':
+//             return A / B;
+//         default:
+//             return 0
+//     }
+// }
+// console.log(calc(10, 15, "*"));
+// console.log(calc(10, 15, "-"));
+// console.log(calc(10, 15, "+"));
+// console.log(calc(10, 15, "/"));
+
+
+// Func-8
+
+// function isEven(K) {
+//     if (K % 2 === 0){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+// console.log(isEven(10));
+// console.log(isEven(11));
+
+// Func-9
+// function sortABC(a, b, c) {
+//     const numbers = [a, b, c]
+//     numbers.sort((x, y) => x - y);
+//     return `${numbers[0]}, ${numbers[1]}, ${numbers[2]}`;
+// }
+// console.log(sortABC(5, 2, 15));
+
+// Func-10
+
